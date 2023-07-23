27960d39734f6a00daa27c193636b0c7753a5f62
Zhiyi Luo <wowo.luo.zhiyi@gmail.com>
2023-07-23 09:29:45 +0200

    Updated code template

--- 00template.cpp
+++ 00template.cpp
@@ -7,10 +7,20 @@ using namespace std;
 #define debug(...) 0
 #endif
 
+#define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define eb emplace_back
+#define pb push_back
+#define mt make_tuple
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define SZ(x) (int) x.size()
+
 using ll = long long;
 using ld = long double;
+using ii = pair<int, int>;
 using vi = vector<int>;
 using vll = vector<ll>;
+using vii = vector<ii>;
 template <class T> using mx_pq = priority_queue<T>;
 template <class T> using mn_pq = priority_queue<T, vector<T>, greater<T>>;
 
