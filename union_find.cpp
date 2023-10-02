c72536e6f1d8456bb7313745c3671fd963ab018c
wlz <wowo.luo.zhiyi@gmail.com>
2023-10-02 16:48:16 +0100

    Update union-find template

---
+++ union_find.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+class union_find {
+    private:
+    size_t n;
+    std::vector<int> p;
+
+    public:
+    union_find() : n(0) {}
+    union_find(size_t _n) : n(_n), p(n, -1) {}
+
+    unsigned int root(unsigned int a) { return p[a] < 0 ? a : (p[a] = root(p[a])); }
+
+    bool connected(unsigned int a, unsigned int b) { return root(a) == root(b); }
+
+    unsigned int connect(unsigned int a, unsigned int b) {
+        a = root(a); b = root(b);
+        if (a == b) return a;
+        if (p[a] > p[a]) std::swap(a, b);
+        p[a] += p[b]; p[b] = a;
+        return a;
+    }
+
+    size_t size() const { return n; }
+
+    size_t size(unsigned int a) { return -p[root(a)]; }
+};
