c8a3a2dc74e708baed6c4d77bc52011273187a11
wlz <wowo.luo.zhiyi@gmail.com>
2023-10-19 11:40:51 +0100

    Update modint

--- debug.hpp
+++ debug.hpp
@@ -1,7 +1,6 @@
 #ifndef DEBUG_HPP
 #define DEBUG_HPP 1
 
-#include "math/modint.hpp"
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,22 +8,14 @@ using namespace std;
 
 #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 
-string to_string(const string &s) {
-  return '"' + s + '"';
-}
+string to_string(const string &s) { return '"' + s + '"'; }
 
-string to_string(const char *s) {
-  return to_string((string) s);
-}
+string to_string(const char *s) { return to_string((string) s); }
 
-string to_string(bool b) {
-  return (b ? "true" : "false");
-}
+string to_string(bool b) { return (b ? "true" : "false"); }
 
 template <typename T1, typename T2>
-string to_string(pair<T1, T2> p) {
-  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
-}
+string to_string(pair<T1, T2> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
 
 template<typename T>
 string to_string(const priority_queue<T> &pq) {
@@ -69,11 +60,6 @@ string to_string(const queue<T> &q) {
   }
   ans += '}';
   return ans;
-}
-
-template<const int MOD>
-string to_string(const modint<MOD> &x) {
-    return to_string(x.val());
 }
 
 template <typename A>
--- math/modint.hpp
+++ math/modint.hpp
@@ -1,17 +1,17 @@
 #ifndef MATH_MODINT_HPP
 #define MATH_MODINT_HPP 1
-
+ 
 #include <algorithm>
 #include <iostream>
 #include <utility>
 #include <tuple>
-
+ 
 /**
  * Compute the greatest common divisor d of a and b together 
- * with the x and y such that ax + by = d using the extended Euclidean algorithm
+ * with some x and y such that ax + by = d using the extended Euclidean algorithm.
  * 
- * @param integers a, b whose gcd will be computed, references x, y where values will be stored
- * @returns greatest common divisor of a and b
+ * @param integers a, b whose gcd will be computed, references x, y where values will be stored.
+ * @returns Greatest common divisor of a and b.
 */
 template<typename T1, typename T2>
 T1 extgcd(const T1& a, const T1& b, T2 &x, T2 &y) {
@@ -26,7 +26,7 @@ T1 extgcd(const T1& a, const T1& b, T2 &x, T2 &y) {
     }
     return a1;
 }
-
+ 
 /**
  * Modular integer implementation
  * NOT completely tested!
@@ -34,111 +34,130 @@ T1 extgcd(const T1& a, const T1& b, T2 &x, T2 &y) {
 template<const int MOD>
 class modint {
     private: int x;
-
+ 
     public:
     modint() : x(0) {}
-
+ 
     template<typename T>
-    void set(const T _x, bool raw = false) {
+    void set(const T &_x, bool raw = false) {
         if (raw || (0 <= _x && _x < MOD)) x = _x;
         else {
             x = _x % MOD;
             if (x < 0) x += MOD;
         }
     }
-
+ 
     template<typename T>
-    modint(const T &_x, bool raw = false) {
-        set(_x, raw);
-    }
-
+    modint(const T &_x, bool raw = false) { set(_x, raw); }
+ 
     template<typename T>
-    modint<MOD> &operator=(const T &_x) {
-        set(_x);
-        return *this;
-    }
-
-    modint<MOD> &operator+=(const modint<MOD> &rhs) {
+    modint &operator=(const T &_x) { set(_x); return *this; }
+ 
+    modint &operator+=(const modint &rhs) {
         x += rhs.x;
         if (x >= MOD) x -= MOD;
         return *this;
     }
-
-    friend modint<MOD> operator+(modint<MOD> lhs, const modint<MOD> &rhs) {
-        lhs += rhs;
-        return lhs;
-    }
-
-    modint<MOD> &operator-=(const modint<MOD> &rhs) {
+ 
+    template<typename T> 
+    modint &operator+=(const T &rhs) { return operator+=(modint(rhs)); }
+ 
+    modint &operator-=(const modint &rhs) {
         x -= rhs.x;
         if (x < 0) x += MOD;
         return *this;
     }
-
-    friend modint<MOD> operator-(modint<MOD> lhs, const modint<MOD> &rhs) {
-        lhs -= rhs;
-        return lhs;
-    }
-
-    modint<MOD> &operator*=(const modint<MOD> &rhs) {
+ 
+    template<typename T> 
+    modint &operator-=(const T &rhs) { return operator-=(modint(rhs)); }
+ 
+    modint &operator*=(const modint &rhs) { 
         x = (unsigned long long) x * rhs.x % MOD;
         return *this;
     }
-
-    friend modint<MOD> operator*(modint<MOD> lhs, const modint<MOD> &rhs) {
-        lhs *= rhs;
-        return lhs;
+ 
+    template<typename T> 
+    modint &operator*=(const T &rhs) { return operator*=(modint(rhs)); }
+ 
+    modint inv() const {
+        modint x1, y1; extgcd(x, MOD, x1, y1);
+        return x1;
     }
-
-    modint<MOD> inv() const {
-        modint x1, y1;
-        extgcd(x, MOD, x1, y1);
-        return std::move(x1);
-    }
-
-    modint<MOD> &operator/=(const modint<MOD> &rhs) {
+ 
+    modint &operator/=(const modint &rhs) {
         operator*=(rhs.inv());
         return *this;
     }
-
-    friend modint<MOD> operator/(modint<MOD> lhs, const modint<MOD> &rhs) {
-        lhs /= rhs;
-        return lhs;
-    }
-
-    modint<MOD> &operator%=(const modint<MOD> &rhs) {
+ 
+    template<typename T> 
+    modint &operator/=(const T &rhs) { return operator/=(modint(rhs)); }
+ 
+    modint &operator%=(const modint &rhs) {
         x %= rhs.val();
         return *this;
     }
-
-    friend modint<MOD> operator%(modint<MOD> lhs, const modint<MOD> &rhs) {
-        lhs %= rhs;
-        return lhs;
+ 
+    template<typename T> 
+    modint &operator%=(const T &rhs) { return operator%=(modint(rhs)); }
+ 
+    modint &operator++() {
+        if (++x == MOD) x = 0;
+        return *this;
     }
-
-    friend bool operator==(const modint<MOD> &lhs, const modint<MOD> &rhs) {
-        return lhs.val() == rhs.val();
+ 
+    modint operator++(int) {
+        modint tmp(*this); operator++();
+        return tmp;        
     }
-
-    friend bool operator!=(const modint<MOD> &lhs, const modint<MOD> &rhs) {
-        return !(lhs == rhs);
+ 
+    modint &operator--() {
+        if (--x < 0) x = MOD - 1;
+        return *this; 
     }
-
-    friend bool operator< (const modint<MOD> &lhs, const modint<MOD> &rhs) { return lhs.val() < rhs.val(); }
-    friend bool operator> (const modint<MOD> &lhs, const modint<MOD> &rhs) { return rhs < lhs; }
-    friend bool operator<=(const modint<MOD> &lhs, const modint<MOD> &rhs) { return !(lhs > rhs); }
-    friend bool operator>=(const modint<MOD> &lhs, const modint<MOD> &rhs) { return !(lhs < rhs); }
-
+ 
+    modint operator--(int) {
+        modint tmp(*this); operator--();
+        return tmp;
+    }
+ 
+#if __cplusplus >= 202002L
+    inline auto operator<=>(const modint&) const = default;
+#endif
+ 
     template<typename T>
-    operator T() const {
-        return x;
-    }
-
-    int val() const {
-        return x;
-    }
+    operator T() const { return x; }
+ 
+    int val() const { return x; }
 };
-
+ 
+template<const int MOD, typename T>
+inline modint<MOD> operator+(const modint<MOD> &lhs, const T &rhs) { modint<MOD> tmp = lhs; tmp += rhs; return tmp; }
+template<const int MOD, typename T>
+inline modint<MOD> operator-(const modint<MOD> &lhs, const T &rhs) { modint<MOD> tmp = lhs; tmp -= rhs; return tmp; }
+template<const int MOD, typename T>
+inline modint<MOD> operator*(const modint<MOD> &lhs, const T &rhs) { modint<MOD> tmp = lhs; tmp *= rhs; return tmp; }
+template<const int MOD, typename T>
+inline modint<MOD> operator/(const modint<MOD> &lhs, const T &rhs) { modint<MOD> tmp = lhs; tmp /= rhs; return tmp; }
+template<const int MOD, typename T>
+inline modint<MOD> operator%(const modint<MOD> &lhs, const T &rhs) { modint<MOD> tmp = lhs; tmp %= rhs; return tmp; }
+ 
+#if __cplusplus < 202002L
+ 
+template<const int MOD>
+inline bool operator==(const modint<MOD> &lhs, const modint<MOD> &rhs) { return lhs.val() == rhs.val(); }
+template<const int MOD>
+inline bool operator!=(const modint<MOD> &lhs, const modint<MOD> &rhs) { return !(lhs == rhs); }
+template<const int MOD>
+inline bool operator< (const modint<MOD> &lhs, const modint<MOD> &rhs) { return lhs.val() < rhs.val(); }
+template<const int MOD>
+inline bool operator> (const modint<MOD> &lhs, const modint<MOD> &rhs) { return rhs < lhs; }
+template<const int MOD>
+inline bool operator<=(const modint<MOD> &lhs, const modint<MOD> &rhs) { return !(lhs > rhs); }
+template<const int MOD>
+inline bool operator>=(const modint<MOD> &lhs, const modint<MOD> &rhs) { return !(lhs < rhs); }
+ 
+#endif
+ 
 template<const int MOD, typename T>
 modint<MOD> pow(const modint<MOD> &_a, const T &_b) {
     modint<MOD> ans(1, true), a(_a, true);
@@ -150,7 +169,7 @@ modint<MOD> pow(const modint<MOD> &_a, const T &_b) {
     }
     return ans;
 }
-
+ 
 template<const int MOD>
 std::istream &operator>>(std::istream &is, modint<MOD> &x) {
     unsigned int _x;
@@ -158,14 +177,14 @@ std::istream &operator>>(std::istream &is, modint<MOD> &x) {
     x.set(_x, true);
     return is;
 }
-
+ 
 template<const int MOD>
 std::ostream &operator<<(std::ostream &os, const modint<MOD> &x) {
     os << x.val();
     return os;
 }
-
-using mint1000000007 = modint<1000000007>;
-using mint998244353 = modint<998244353>;
-
+ 
+using modint1000000007 = modint<1000000007>;
+using modint998244353 = modint<998244353>;
+ 
 #endif // MATH_MODINT_HPP
