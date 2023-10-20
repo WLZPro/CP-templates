9dd6f352fa1e3d51d107739416f69f07c7d8b43c
wlz <wowo.luo.zhiyi@gmail.com>
2023-10-20 13:09:40 +0100

    Add getline to fast IO

--- util/fast_io.hpp
+++ util/fast_io.hpp
@@ -1,3 +1,18 @@
+/**
+ * Faster C++ IO using fread and frwite.
+ * Use in problems where IO is the bottleneck
+ * (usually when the IO size is on the order 
+ * of 10^6).
+ * 
+ * IMPORTANT: Paste/include this file AFTER any 
+ * include of iostream, or otherwise the code 
+ * won't compile.
+ * 
+ * Note: ios::sync_with_stdio(false) and cin.tie(0)
+ * should be kept, since they are redefined to 
+ * initialize the input/output.
+ */
+
 #ifndef UTIL_FAST_IO_HPP
 #define UTIL_FAST_IO_HPP 1
 
@@ -5,6 +20,7 @@
 #include <cstdio>
 #include <cctype>
 #include <cstdlib>
+#include <string>
 
 namespace fast_input {
 
@@ -12,19 +28,25 @@ namespace fast_input {
     char buf[BUF_SZ];
     int pos = 0, len = 0;
 
+    struct fast_reader { void tie(int *x) {} } fcin;
+
     #define rd (pos == len ? (pos = 0, (!(len = (int) fread(buf, 1, BUF_SZ, stdin)) ? EOF : buf[pos++])) : buf[pos++])
 
     template<typename T>
-    void read(T &x) {
+    inline void read(T &x) {
         short sgn = 1; char c;
         while (!std::isdigit(c = rd)) if (c == '-') sgn = -sgn;
         x = c - '0';
         while (std::isdigit(c = rd)) { x = x * 10 + (c - '0'); }
     }
 
-    #undef rd
+    fast_reader &getline(fast_reader &in, std::string &s) {
+        s.clear(); char c;
+        while ((c = rd) != '\n') s += c;
+        return in;
+    }
 
-    struct fast_reader { void tie(int *x) {} } fcin;
+    #undef rd
 } // namespace fast_input
 
 template<typename T>
@@ -36,13 +58,15 @@ namespace fast_output {
     char buf[BUF_SZ];
     int pos;
 
+    struct fast_writer {} fcout;
+
     void flush() { fwrite(buf, 1, pos, stdout); pos = 0; }
     void sync_with_stdio(bool b) { assert(std::atexit(flush) == 0); }
 
     #define wt(c) ((pos == BUF_SZ ? flush() : (void) 0), buf[pos++] = (c))
 
     template<typename T>
-    void write(const T &y) {
+    inline void write(const T &y) {
         T x = y;
         static char num_buf[100];
         if (x < 0) wt('-'), x = -x;
@@ -53,11 +77,9 @@ namespace fast_output {
     }
 
     template<>
-    void write(const char &c) { wt(c); }
+    inline void write(const char &c) { wt(c); }
 
     #undef wt
-
-    struct fast_writer {} fcout;
 } // namespace fast_output
 
 template<typename T>
@@ -66,5 +88,6 @@ fast_output::fast_writer &operator<<(fast_output::fast_writer &out, const T &x) 
 #define cin fast_input::fcin
 #define ios fast_output
 #define cout fast_output::fcout
+#define getline fast_input::getline
 
 #endif // UTIL_FAST_IO_HPP
