0155e75b27edc0e1d9131f09a4a17159af1cf38e
wlz <wowo.luo.zhiyi@gmail.com>
2023-10-26 18:06:07 +0100

    Add some operations to modint

--- math/modint.hpp
+++ math/modint.hpp
@@ -27,6 +27,8 @@ class modint {
     private: int x;
 
     public:
+    
+    // Assignment/constuctors
     modint() : x(0) {}
 
     template<typename T>
@@ -44,28 +46,36 @@ class modint {
     template<typename T>
     constexpr modint &operator=(const T &_x) { set(_x); return *this; }
 
+    // Binary arithmetic operators
     constexpr modint &operator+=(const modint &rhs) { x += rhs.x; if (x >= MOD) x -= MOD; return *this; }
-
     constexpr friend modint operator+(modint lhs, const modint &rhs) { return lhs += rhs; }
 
     constexpr modint &operator-=(const modint &rhs) { x -= rhs.x; if (x < 0) x += MOD; return *this; }
-
     constexpr friend modint operator-(modint lhs, const modint &rhs) { return lhs -= rhs; }
 
     constexpr modint &operator*=(const modint &rhs) { x = (unsigned long long) x * rhs.x % MOD; return *this; }
-
     constexpr friend modint operator*(modint lhs, const modint &rhs) { return lhs *= rhs; }
 
     constexpr modint inv() const { modint x1, y1; extgcd(x, MOD, x1, y1); return x1; }
 
     constexpr modint &operator/=(const modint &rhs) { operator*=(rhs.inv()); return *this; }
-
     constexpr friend modint operator/(modint lhs, const modint &rhs) { return lhs /= rhs; }
 
     constexpr modint &operator%=(const modint &rhs) { x %= rhs.val(); return *this; }
-
     constexpr friend modint operator%(modint lhs, const modint &rhs) { return lhs %= rhs; }
 
+    // Unary arithemtic operators
+    constexpr modint operator-() { return modint(MOD - x, true); }
+    constexpr modint operator+() { return *this; }
+
+    // Increment/decrement
+    constexpr modint &operator++() { if (++x == MOD) x = 0; return *this; }
+    constexpr modint operator++(int) { modint cpy(*this); operator++(); return cpy; }
+
+    constexpr modint &operator--() { if (--x < 0) x = MOD - 1; return *this; }
+    constexpr modint operator--(int) { modint cpy(*this); operator--(); return cpy; }
+
+    // Comparison operators
 #if __cplusplus < 202002L
     constexpr friend bool operator==(const modint &lhs, const modint &rhs) { return lhs.val() == rhs.val(); }
     constexpr friend bool operator!=(const modint &lhs, const modint &rhs) { return !(lhs == rhs); }
@@ -77,39 +87,37 @@ class modint {
     constexpr auto operator<=>(const modint&) const = default;
 #endif
 
+    // Exponentiation
+    // https://cp-algorithms.com/algebra/binary-exp.html#applications
+    template<typename T>
+    friend modint pow(const modint &_a, const T &_b) {
+        modint ans(1, true), a(_a, true); T b(_b);
+        if (b < 0) a = a.inv(), b = -b;
+        while (b > 0) {
+            if (b & 1) ans *= a;
+            a *= a;
+            b >>= 1;
+        }
+        return ans;
+    }
+
+    // Input/output
+    friend std::istream &operator>>(std::istream &is, modint &m) { is >> m.x; return is; }
+    friend std::ostream &operator<<(std::ostream &os, const modint &m) { os << m.x; return os; }
+
+    // Debugging
     constexpr friend std::string to_string(const modint &m) { return to_string(m.x); }
 
+    // Casting
     template<typename T>
     constexpr operator T() const { return x; }
 
+    // Value extraction
     constexpr int val() const { return x; }
 };
 
-template<const int MOD, typename T>
-modint<MOD> pow(const modint<MOD> &_a, const T &_b) {
-    modint<MOD> ans(1, true), a(_a, true);
-    T b(_b);
-    while (b > 0) {
-        if (b & 1) ans *= a;
-        a *= a;
-        b >>= 1;
-    }
-    return ans;
-}
-
-template<const int MOD>
-std::istream &operator>>(std::istream &is, modint<MOD> &x) {
-    unsigned int _x;
-    is >> _x;
-    x.set(_x, true);
-    return is;
-}
-
-template<const int MOD>
-std::ostream &operator<<(std::ostream &os, const modint<MOD> &x) {
-    os << x.val();
-    return os;
-}
+template<const int MOD, typename T, typename S>
+modint<MOD> mpow(const T &a, const S &b) { return pow(modint<MOD>(a), b); }
 
 using modint1000000007 = modint<1000000007>;
 using modint998244353 = modint<998244353>;
