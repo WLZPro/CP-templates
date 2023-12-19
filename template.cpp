cd6f28dc667aa0a043381a5e7fbdd7e2630b0aa9
wlz <wowo.luo.zhiyi@gmail.com>
2023-12-19 08:19:12 +0800

    Update template

--- template.cpp
+++ template.cpp
@@ -4,40 +4,40 @@ using namespace std;
 #ifdef DEBUG
 #include "debug.hpp"
 #else
-#define debug(...) 0
+#define debug(...)
 #endif
 
-#define rep(i,a,b) for(auto i=(a)-((a)>(b));i!=(b)-((a)>(b));i+=1-2*((a)>(b)))
+#define rep(i, a, b) for (auto i = (a); i < (b); i++)
+#define repr(i, a, b) for (auto i = (a) - 1; i >= (b); i--)
 #define eb emplace_back
 #define pb push_back
 #define MP make_pair
 #define MT make_tuple
 #define all(x) begin(x), end(x)
-#define SZ(x) (int) x.size()
+#define SZ(x) static_cast<int>(x.size())
 
 using ll = long long;
 using ull = unsigned ll;
-using lll = __int128_t;
-using ulll = __uint128_t;
-using ld = long double;
+// using lll = __int128_t;
+// using ulll = __uint128_t;
+
 using ii = pair<int, int>;
 using vi = vector<int>;
 using vll = vector<ll>;
 using vii = vector<ii>;
 
-template<typename T> void cmax(T &a, const T &b) { a = max(a, b); }
-template<typename T> void cmin(T &a, const T &b) { a = min(a, b); }
+template<typename T> constexpr bool cmax(T &a, const T &b) { return a < b ? a = b, true : false; }
+template<typename T> constexpr bool cmin(T &a, const T &b) { return b < a ? a = b, true : false; }
 
-constexpr int INF = 0x3f3f3f3f;
-constexpr ll LINF = (ll) 1e18;
-constexpr double DINF = numeric_limits<double>::infinity();
-constexpr double EPS = 1e-9;
+constexpr int inf = 0x3f3f3f3f;
+constexpr ll linf = (ll) 1e18;
+constexpr double dinf = numeric_limits<double>::infinity();
+constexpr double eps = 1e-9;
 
 void solve();
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    ios::sync_with_stdio(false); cin.tie(nullptr);
     int t = 1;
     //cin >> t;
     for (int q = 1; q <= t; q++) solve();
