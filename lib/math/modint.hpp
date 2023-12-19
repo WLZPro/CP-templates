b21c84e30ee1ac62aff426fed9fbdcd94c42671b
wlz <wowo.luo.zhiyi@gmail.com>
2023-12-19 08:24:39 +0800

    Add some static functions to modint

--- lib/math/modint.hpp
+++ lib/math/modint.hpp
@@ -5,12 +5,12 @@
 #include "math/mod_operations.hpp"
 #include "math/mod_inverse.hpp"
 
-template<unsigned int md>
+template<uint32_t md>
 class modint {
     using mint = modint;
 
     public:
-    static constexpr unsigned int mod = md;
+    static constexpr uint32_t mod = md;
 
     constexpr modint() : x(0) {}
 
@@ -22,15 +22,15 @@ class modint {
 
     template<typename T>
     constexpr void set(const T &_x, bool raw = false) {
-        if (raw || (0 <= _x && static_cast<unsigned int>(_x) < md)) x = _x;
+        if (raw || (0 <= _x && static_cast<uint32_t>(_x) < md)) x = _x;
         else {
             long long tmp = _x % md;
             if (tmp < 0) tmp += md;
-            x = (unsigned int) tmp;
+            x = static_cast<uint32_t>(tmp);
         }
     }
 
-    constexpr unsigned int val() const { return x; }
+    constexpr int val() const { return static_cast<int>(x); }
 
     template<typename T>
     constexpr operator T() const { return (T) x; }
@@ -84,7 +84,7 @@ class modint {
     constexpr mint operator+() const { return *this; }
     constexpr mint operator-() const { return mint(md - x, true); }
 
-    constexpr mint inv() const { return invs_mod<md>::get(x); }
+    constexpr mint inv() const { return inv(x); }
 
     constexpr friend bool operator==(const mint &a, const mint &b) { return a.x == b.x; }
     constexpr friend bool operator!=(const mint &a, const mint &b) { return a.x != b.x; }
@@ -100,8 +100,43 @@ class modint {
 
     inline friend std::string to_string(const mint &m) { return std::to_string(m.x); }
 
+    static inline int invs_up_to = 1, fact_up_to = 1;
+
+    static void compute_inverses(int n) {
+        if (n <= invs_up_to) return;
+        _inv.resize(n + 1);
+        for (uint32_t a = invs_up_to + 1; a <= static_cast<uint32_t>(n); a++) _inv[a] = md - mul_mod(md / a, _inv[md % a], md);
+        invs_up_to = n;
+    }
+
+    static void compute_factorials(int n) {
+        compute_inverses(n);
+        _fact.resize(n + 1); _inv_fact.resize(n + 1);
+        for (uint32_t a = fact_up_to + 1; a <= static_cast<uint32_t>(n); a++) {
+            _fact[a] = mul_mod(_fact[a - 1], a, md);
+            _inv_fact[a] = mul_mod(_inv_fact[a - 1], _inv[a], md);
+        }
+        fact_up_to = n;
+    }
+
+    static mint inv(int n) {
+        if (n <= invs_up_to) return mint(_inv[n], true);
+        return mint(inv_mod<uint32_t, md>(static_cast<uint32_t>(n)), true);
+    }
+    static mint fact(int n) { compute_factorials(n); return mint(fact[n], true); }
+    static mint inv_fact(int n) { compute_factorials(n); return mint(inv_fact[n], true); }
+
+    static mint binom(int n, int k) {
+        if (k < 0 || k > n) return mint(0, true);
+        return fact(n) * inv_fact(k) * inv_fact(n - k);
+    }
+
+    static mint catalan(int n) { return binom(2 * n, n) * inv(n + 1); }
+
     private:
-    unsigned int x;
+    uint32_t x;
+
+    static inline std::vector<uint32_t> _inv = {0, 1}, _fact = {1, 1}, _inv_fact = {1, 1};
 };
 
 using modint1000000007 = modint<1000000007>;
